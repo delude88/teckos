b0b86e6787e1324697b4f6a028f4da7b6318765c
delude88 <tobias.hegemann@googlemail.com>
2021-03-29 10:52:40 +0200

    updated uwebsocket.js

--- npm-scripts.js
+++ npm-scripts.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const Axios = require('axios');
 const AdmZip = require('adm-zip');
 
-const U_WEBSOCKET_VERSION = "18.10.0";
+const U_WEBSOCKET_VERSION = "18.14.0";
 
 const download = async (url, dest) => {
     const writer = fs.createWriteStream(dest)

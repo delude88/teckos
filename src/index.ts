9871689f8f09ef7acceefc48fc034c191ff7c377
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-09-08 17:40:15 +0200

    Fixed es generation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {ITeckosProvider} from './types/ITeckosProvider'
-import {ITeckosSocket} from './types/ITeckosSocket'
-import {TeckosPacket} from './types/TeckosPacket'
-import {UWSProvider} from './UWSProvider'
-import {UWSSocket} from './UWSSocket'
-import uws, {WebSocket, TemplatedApp} from 'uWebSockets.js'
+import { ITeckosProvider } from './types/ITeckosProvider'
+import { ITeckosSocket } from './types/ITeckosSocket'
+import { TeckosPacket } from './types/TeckosPacket'
+import { UWSProvider } from './UWSProvider'
+import { UWSSocket } from './UWSSocket'
+import uws, { WebSocket, TemplatedApp } from 'uWebSockets.js'
 
 const App = uws.App
 const SSLApp = uws.SSLApp
 
-export type {TeckosPacket, ITeckosSocket, ITeckosProvider, WebSocket, TemplatedApp}
-export {UWSSocket, UWSProvider, App, SSLApp}
+export type { TeckosPacket, ITeckosSocket, ITeckosProvider, WebSocket, TemplatedApp }
+export { UWSSocket, UWSProvider, App, SSLApp }

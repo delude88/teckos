3f212a78ae1ad06aa9e4bfc4a96400a396651a78
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-09-28 20:11:26 +0200

    Enhanced ES

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,12 +1,12 @@
 import { config } from 'dotenv';
-import {UWSProvider, App} from 'teckos';
+import {UWSProvider, uws} from 'teckos';
 
 config();
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 const start = async () => {
-  const app = App()
+  const app = uws.App()
   const io = new UWSProvider(app, {
     redisUrl: process.env.REDIS_URL,
     pingInterval: 2000,
